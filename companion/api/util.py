942484e395b5d168b1ae4b4680d105bc6d98e93d
David Volquartz Lebech <david@lebech.info>
2015-11-30 16:26:21 +0100

    Use zip-files as the default backup storage type
    This commit changes the default storage type for backup files to
    zip-files rather than tar-files to allow incrementally creating the
    compressed archive rather than downloading everything before
    compressing.
    
    Changes:
    - Add utility function to zip a directory.
    - Add functionality for fetching and zip'ing files incrementally.
    - Add tests for backup API.

--- companion/api/util.py
+++ companion/api/util.py
@@ -1,7 +1,9 @@
 """Common utility functions used across commands."""
 import os
 import json
+import shutil
 import tarfile
+import zipfile
 
 import certifi
 import elasticsearch
@@ -38,3 +40,50 @@ def tar_gz_directory(directory, target_path):
     with tarfile.open(tar_path, 'w:gz') as tar:
         tar.add(directory, directory_name)
     return tar_path
+
+
+def zip_directory(directory, target_path, append=False, delete_original=False):
+    """Zip the contents of a single directory.
+
+    The zip-file will have the same name as the basename of the directory.
+
+    :param directory: The directory to tar the contents of.
+    :type directory: str
+    :param target_path: The target of the compressed tar file.
+    :type target_path: str
+    :param append: Determine whether or not to append to an existing archive. If
+        False, a new archive will always be created. Default is False.
+    :type append: bool
+    :param delete_original: Determine whether or not to delete the directory
+        that was zipped. Default is False.
+    :type append: bool
+    :returns: The full path to the created zip file.
+
+    """
+    full_directory = os.path.abspath(os.path.normpath(directory))
+    directory_name = os.path.basename(full_directory)
+    zip_path = '{}/{}.zip'.format(target_path, directory_name)
+    if os.path.exists(zip_path) and not append:
+        os.remove(zip_path)
+
+    mode = 'a' if append else 'w'
+    with zipfile.ZipFile(zip_path, mode=mode,
+                         compression=zipfile.ZIP_DEFLATED) as zf:
+        for dirpath, _, filenames in os.walk(full_directory):
+            for filename in filenames:
+                # Find the full filepath for the filename
+                # For storing the file in the zip, remove the part of the
+                # full path that contains directory structures above the
+                # directory being zipped
+                # For example, /home/user/mydirectory/myfile.txt becomes
+                # mydirectory/myfile.txt in the zipfile.
+                full_filepath = os.path.join(dirpath, filename)
+                short_filepath = os.path.relpath(full_filepath,
+                                                 start=full_directory)
+                short_filepath = os.path.join(directory_name, short_filepath)
+                zf.write(full_filepath, short_filepath)
+
+    if delete_original:
+        shutil.rmtree(directory)
+
+    return zip_path
--- test/test_api_util.py
+++ test/test_api_util.py
@@ -1,7 +1,9 @@
 """Util test functions."""
 import os
+import uuid
 import shutil
 import tarfile
+import zipfile
 import tempfile
 from unittest import TestCase
 
@@ -61,3 +63,95 @@ class TestTarGzDirectory(TestCase):
         with open(check_filename) as f:
             s = f.read()
             self.assertEqual(s, 'This string will be compressed')
+
+
+class TestZipDirectory(TestCase):
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.compressdir = os.path.join(self.tmpdir, 'test/')
+        os.mkdir(self.compressdir)
+        self.create_file()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def create_file(self, filename=None):
+        if filename:
+            self.filename = filename
+        else:
+            self.filename = '{}.txt'.format(uuid.uuid4())
+        self.filepath = os.path.join(self.compressdir, self.filename)
+
+        # If the directory containing the full filepath does not exist, create
+        # all intermediary directories.
+        if not os.path.exists(os.path.dirname(self.filepath)):
+            os.makedirs(os.path.dirname(self.filepath))
+
+        with open(self.filepath, 'w') as f:
+            f.write('This string will be compressed')
+
+    def test_zip(self):
+        """It should compress a file and be able to decompress."""
+        zip_path = util.zip_directory(self.compressdir, self.tmpdir)
+
+        # Zip file should be smaller
+        self.assertLess(os.path.getsize(self.filepath),
+                        os.path.getsize(zip_path))
+
+        # Zip file should be able to decompress and have same contents as
+        # before.
+        checkdir = os.path.join(self.tmpdir, 'check/')
+        os.mkdir(checkdir)
+        check_filename = os.path.join(checkdir, 'test/{}'.format(self.filename))
+        with zipfile.ZipFile(zip_path) as f:
+            f.extractall(checkdir)
+
+        with open(check_filename) as f:
+            s = f.read()
+            self.assertEqual(s, 'This string will be compressed')
+
+    def test_zip_multi_level(self):
+        """It should support multiple directory levels."""
+        self.create_file('test1/test.txt')
+        self.create_file('test1/testsubdir/test.txt')
+        self.create_file('test2/test.txt')
+        zip_path = util.zip_directory(self.compressdir, self.tmpdir)
+        with zipfile.ZipFile(zip_path) as f:
+            names = f.namelist()
+            self.assertIn('test/test1/test.txt', names)
+            self.assertIn('test/test1/testsubdir/test.txt', names)
+            self.assertIn('test/test2/test.txt', names)
+
+    def test_zip_no_delete_original(self):
+        """It should not delete the original files by default."""
+        util.zip_directory(self.compressdir, self.tmpdir)
+        self.assertTrue(os.path.exists(self.filepath))
+
+    def test_zip_delete_original(self):
+        """It should delete the original files if specified."""
+        util.zip_directory(self.compressdir, self.tmpdir,
+                           delete_original=True)
+        self.assertFalse(os.path.exists(self.filepath))
+
+    def test_zip_no_append(self):
+        """It should not append files by default."""
+        util.zip_directory(self.compressdir, self.tmpdir,
+                           delete_original=True)
+        self.create_file()
+        zip_path = util.zip_directory(self.compressdir, self.tmpdir)
+        with zipfile.ZipFile(zip_path) as f:
+            self.assertEqual(len(f.namelist()), 1)
+
+    def test_zip_append(self):
+        """It should append files if specified."""
+        zip_path = util.zip_directory(self.compressdir, self.tmpdir,
+                                      delete_original=True, append=True)
+        with zipfile.ZipFile(zip_path) as f:
+            self.assertEqual(len(f.namelist()), 1)
+        self.create_file()
+        self.create_file()
+        self.create_file()
+        zip_path = util.zip_directory(self.compressdir, self.tmpdir,
+                                      append=True)
+        with zipfile.ZipFile(zip_path) as f:
+            self.assertEqual(len(f.namelist()), 4)
