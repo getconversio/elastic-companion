8695d2db19dd63df420aeffa9b761630c9d1d1db
David Volquartz Lebech <david@lebech.info>
2016-03-07 12:04:14 +0100

    Release 1.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='elastic-companion',
-    version='1.1.0',
+    version='1.2.0',
     description='Useful commands for Elasticsearch',
     long_description=long_description,
     url='https://github.com/receiptful/elastic-companion',
