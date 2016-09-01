8ca32562bf04103b43fffa77e3c421df0d1495a0
David Volquartz Lebech <david@lebech.info>
2016-09-01 10:32:30 +0200

    Release 1.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='elastic-companion',
-    version='1.3.0',
+    version='1.4.0',
     description='Useful commands for Elasticsearch',
     long_description=long_description,
     url='https://github.com/receiptful/elastic-companion',
@@ -24,10 +24,10 @@ setup(
     keywords='elasticsearch cli',
     packages=['companion', 'companion.api', 'companion.cli'],
     install_requires=[
-        'elasticsearch>=1.0.0,<2.0.0',
-        'certifi==2015.4.28',
-        'boto3==1.1.3',
-        'python-dateutil==2.4.2'
+        'elasticsearch>=1.9.0,<2.0.0',
+        'certifi==2016.8.31',
+        'boto3==1.4.0',
+        'python-dateutil==2.5.3'
     ],
     extras_require={
         'dev': ['twine', 'wheel', 'nose', 'coverage']
